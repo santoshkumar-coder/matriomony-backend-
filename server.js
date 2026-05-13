31d85ad4f07697164cd7f0ce307e4a9912256525
santoshkumar-coder <santosh.kumar@pearlorganisation.com>
2026-05-13 11:00:00 +0530

    add Swagger documentation for user management APIs and integrate Swagger UI

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 require('dotenv').config();
 const connectDB = require("./src/config/db");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const http = require("http");
 const app = require("./src/app");
 const { initSocket } = require("./src/socket/socket");
@@ -17,9 +19,37 @@ const io = initSocket(server);
 connectDB(); 
 
 const PORT = process.env.PORT || 5001;
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "VenbaImpex API Documentation",
+      version: "1.0.0",
+      description: "API documentation",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  // apis: ["./routes/*.js"], // path to route files
+  apis: ["./src/routes/*.js"],
+};
 
-server.listen(PORT, () => {
-  console.log(`Service running on ${PORT}`);
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(PORT, () => {
+  console.log(`
+--------------------------------
+|| Service running on ${PORT} ||
+--------------------------------
+
+Swagger Docs:
+http://localhost:${PORT}/api-docs
+`);
 });
 
 module.exports = { io };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,7 +4,33 @@ const validate = require("../middlewares/validate");
 const { createUserSchema } = require("../validators/user.validator");
 const upload = require('../config/multer'); 
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management APIs
+ */
+
+
+/**
+ * @swagger
+ * /api/v1/user/create:
+ *   post:
+ *     summary: create new user
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.post('/create', validate(createUserSchema), upload.array('images', 5), userController.createUser);
+/**
+ * @swagger
+ * /api/v1/user/get-all-users:
+ *   get:
+ *     summary: get all users
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/get-all-users', userController.getAllUsers);
 
 
